a494d4e48b315c632f1b21aa097d1e463f19a29b
Arslan <muhammadarslanhayat7@gmail.com>
2024-03-17 14:12:26 +0500

    Add build

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import App from './App'
 const root = createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-          <BrowserRouter>
-        <App />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 )
